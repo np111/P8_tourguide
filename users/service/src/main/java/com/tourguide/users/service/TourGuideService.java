9bf6a154f34cd88a9b6c9c2b916a3948a202cb6a
np111 <oc.np111@protonmail.com>
2021-02-25 08:54:40 +0100

    Prepare TourGuideController

---
+++ users/service/src/main/java/com/tourguide/users/service/TourGuideService.java
@@ -0,0 +1,41 @@
+package com.tourguide.users.service;
+
+import com.tourguide.gps.model.Location;
+import com.tourguide.gps.model.VisitedLocation;
+import com.tourguide.users.model.TripDeal;
+import com.tourguide.users.model.UserNearbyAttractions;
+import com.tourguide.users.model.UserReward;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+@Service
+public class TourGuideService {
+    private final UserService userService;
+
+    public VisitedLocation getUserLocation(String userName) {
+        return userService.getUserLocation(userName).orElse(null);
+    }
+
+    public List<UserReward> getUserRewards(String userName) {
+        return userService.getUserRewards(userName);
+    }
+
+    public Map<UUID, Location> getAllCurrentLocations() {
+        return userService.getAllCurrentLocations();
+    }
+
+    public UserNearbyAttractions getNearbyAttractions(String userName) {
+        // TODO
+        return null;
+    }
+
+    public List<TripDeal> getTripDeals(String userName) {
+        // TODO
+        return null;
+    }
+}
